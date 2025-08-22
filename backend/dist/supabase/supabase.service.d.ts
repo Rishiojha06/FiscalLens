bb2178f2be91a29e3a215ccc171c5c32bd7e46f9
Rishi Ojha <rishiojha0606@gmail.com>
2025-08-22 20:31:23 +0530

    revert "Initital Commit"
    This reverts commit 49129a6d85f9f290f23a4c997035adfd136510d0.

--- backend/dist/supabase/supabase.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { SupabaseClient } from '@supabase/supabase-js';
-import { ConfigService } from '@nestjs/config';
-export declare class SupabaseService {
-    private configServive;
-    private supabase;
-    constructor(configServive: ConfigService);
-    getClient(): SupabaseClient;
-}
