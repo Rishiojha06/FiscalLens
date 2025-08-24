f4bead39e9d9bca365c665af9e942e19f7897edd
Rishi Ojha <rishiojha0606@gmail.com>
2025-08-24 18:30:08 +0530

    refactor: enhance auth controller with Swagger documentation and DTO usage

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,12 +1,16 @@
 import { Body, Controller, Post, Get, Query, Redirect } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
+import { SignupDto } from './dtos/signup.dto';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/signup')
-  async signUpWithMail(@Body() body: { email: string; password: string }) {
+  @ApiBody({type: SignupDto})
+  async signUpWithMail(@Body() body: SignupDto) {
     const { email, password } = body;
     return await this.authService.signUpWithEmail(email, password);
   }
